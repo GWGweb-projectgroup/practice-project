7c63afe96ee9267417696b29db01ab2863e857ad
Chris Neal <chrisvneal@gmail.com>
2018-04-21 19:38:39 -1000

    Revising dropdown functionaity

--- js/main.js
+++ js/main.js
@@ -3,13 +3,23 @@ $(document).ready(function() {
 
   // list drop down functionality
 
-  // $('.left-sidebar__list-item.dropdown').on('click', function() {
-  //   $(this).next('li').toggleClass('remove-dropdown');
-  // });
-
   $('.left-sidebar__list-item.dropdown').on('click', function() {
     $(this).next('li').toggle();
   });
+
+
+  
+  // TODO: Change the caret icon when the dropdown list is collapsed
+  // Change the caret icon
+
+
+
+
+
+
+
+  
+
 
 
 
