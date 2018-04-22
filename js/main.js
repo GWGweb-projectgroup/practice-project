8d5b21fa8a6171894aaae47205ab6bb526cdd0a1
Chris Neal <chrisvneal@gmail.com>
2018-04-21 19:04:34 -1000

    Tightened dropdown functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 
 
+  // list drop down functionality
+
+  // $('.left-sidebar__list-item.dropdown').on('click', function() {
+  //   $(this).next('li').toggleClass('remove-dropdown');
+  // });
+
   $('.left-sidebar__list-item.dropdown').on('click', function() {
-    $(this).next('li').toggleClass('remove-dropdown');
+    $(this).next('li').toggle();
   });
-
-// $('.dropdown-list').css('display', 'none');
-
-
 
 
 
